ac10437c4a69b45372d76798f51b16ee4c4d94cd
Andriy <prodavetc@gmail.com>
2019-12-06 17:08:55 -0300

    Fin seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -11,9 +11,17 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient) {
     // console.log('Servicio de infoPagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
 
+
+   }
+
+   private cargarInfo(){
     //  Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
         .subscribe( (resp: InfoPagina) => {
@@ -21,9 +29,22 @@ export class InfoPaginaService {
           this.cargada = true;
           this.info = resp;
 
-          console.log(resp);
-
-        });
+      });
 
    }
+
+   private cargarEquipo(){
+    this.http.get('https://angular-html-dd5eb.firebaseio.com/equipo.json')
+      .subscribe( (resp: any[])  => {
+
+
+        this.equipo = resp;
+
+        console.log(resp);
+
+    });
+   }
+
+
+
 }
